5c39b94a2c2243149ea3a8ecedd0fb555cb00aa4
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-08-20 14:23:54 +0200

    minor adjustment

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../components/auth/AuthProvider';
+import { AuthContext } from '../Components/auth/AuthProvider';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
