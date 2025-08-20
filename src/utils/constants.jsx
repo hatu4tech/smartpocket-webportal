ca1abfa7f29aa4a518bfa7f553af513827e8d4d9
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-08-20 16:08:33 +0200

    Add the server url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api/v1';
+//for development use
+// export const API_BASE_URL = 'http://localhost:8080/api/v1';
+
+//for production use, replace with the actual API base URL
+export const API_BASE_URL = 'http://139.84.236.226:8081/api/v1';
